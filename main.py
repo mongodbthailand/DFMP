d16629a47999ec566db828a3a163dd0384487605
Piti Champeethong <piti_fee@hotmail.com>
2023-08-04 17:46:30 +0800

    upd

--- main.py
+++ main.py
@@ -2,7 +2,22 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/create")
+async def create():
+    return {"message": "Hello Python"}
+
+@app.post("/read")
+async def read():
+    return {"message": "Hello Python"}
+
+@app.post("/update")
+async def update():
+    return {"message": "Hello Python"}
+
+@app.post("/delete")
+async def delete():
+    return {"message": "Hello Python"}
